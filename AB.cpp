2be7c9a12a91208302c756065e71f108c02e8bdd
Patrick Chin <p.ch@live.co.uk>
2017-09-21 22:44:23 +0100

    retab and surround main() with ifdef CHECK

--- AB.cpp
+++ AB.cpp
@@ -1,7 +1,9 @@
 // https://community.topcoder.com/stat?c=problem_statement&pm=13642
 
 #include <string>
+#ifdef CHECK
 #include <cstdio>
+#endif
 #include <cmath>
 
 using namespace std;
@@ -9,52 +11,55 @@ using namespace std;
 class AB
 {
 public:
-	bool checkString(const string s, const int n, const int k)
-	{
-		int sum = 0;
-		for (int i = 0; i < n; ++i) {
-			if (s[i] != 'A')
-				continue;
-			for (int j = i+1; j < n; ++j)
-				if (s[j] == 'B')
-					sum++;
-		}
-		return sum == k;
-	}
+    bool checkString(const string s, const int n, const int k)
+    {
+        int sum = 0;
+        for (int i = 0; i < n; ++i) {
+            if (s[i] != 'A')
+                continue;
+            for (int j = i+1; j < n; ++j)
+                if (s[j] == 'B')
+                    sum++;
+        }
+        return sum == k;
+    }
 
-	string createString(const int n, const int k)
-	{
-		int sq = n*n - 4*k;
-		if (sq < 0)
-			return string();
-		sq = sqrt(sq);
+    string createString(const int n, const int k)
+    {
+        int sq = n*n - 4*k;
+        if (sq < 0)
+            return string();
+        sq = sqrt(sq);
 
-		int a = (n - sq) / 2;
-		int d = k - a*(n-a);
+        int a = (n - sq) / 2;
+        int d = k - a*(n-a);
 
-		if (a < 0 || d < 0)
-			return string();
+        if (a < 0 || d < 0)
+            return string();
 
-		string s = string(a, 'A') + string(n-a, 'B');
+        string s = string(a, 'A') + string(n-a, 'B');
 
-		if (d > 0)
-			s[n-a-d-1] = 'A';
+        if (d > 0)
+            s[n-a-d-1] = 'A';
 
-		bool b = checkString(s, n, k);
-		if (!b) printf("n=%d k=%d: nope s=%s\n", n, k, s.c_str());
+#ifdef CHECK
+        bool b = checkString(s, n, k);
+        if (!b) printf("n=%d k=%d: nope s=%s\n", n, k, s.c_str());
+#endif
 
-		return s;
-	}
+        return s;
+    }
 };
 
-int check()
+#ifdef CHECK
+int main()
 {
-	AB ab;
-	for (int i = 0; i < 50; ++i) {
-		for (int j = 0; j < i*(i+1)/2; ++j) {
-			ab.createString(i, j);
-		}
-	}
-	return 0;
+    AB ab;
+    for (int i = 0; i < 50; ++i) {
+        for (int j = 0; j < i*(i+1)/2; ++j) {
+            ab.createString(i, j);
+        }
+    }
+    return 0;
 }
-
+#endif
--- ABBA.cpp
+++ ABBA.cpp
@@ -1,7 +1,6 @@
 // https://community.topcoder.com/stat?c=problem_statement&pm=13918
 
 #include <string>
-#include <iostream>
 #include <algorithm>
 
 using namespace std;
@@ -9,44 +8,42 @@ using namespace std;
 class ABBA
 {
 public:
+    string canObtain(string const &initial, string target)
+    {
+        bool is_reversed = false;
+        while (target.size() != initial.size())
+        {
+            char c;
+            if (!is_reversed) {
+                c = target.back();
+                target.pop_back();
+            } else {
+                c = target.front();
+                target.erase(0, 1);
+            }
+            if (c == 'B')
+                is_reversed = !is_reversed;
+        }
 
-	string canObtain(string initial, string target)
-	{
-		string reversed = initial;
-		reverse(reversed.begin(), reversed.end());
-		bool is_reversed = false;
-		while (true)
-		{
-			if (target.size() == initial.size())
-			{
-				if ((!is_reversed && target == initial) ||
-					(is_reversed && target == reversed))
-					return string("Possible");
-				else
-					return string("Impossible");
-			}
-			char c;
-			if (!is_reversed) {
-				c = target.back();
-				target.pop_back();
-			} else {
-				c = target.front();
-				target.erase(0, 1);
-			}
-			if (c == 'B')
-				is_reversed = !is_reversed;
-		}
-	}
+        if ((!is_reversed && target == initial) ||
+            (is_reversed && equal(target.begin(), target.end(), initial.rbegin())))
+            return string("Possible");
+        else
+            return string("Impossible");
+    }
 };
+
+#ifdef CHECK
+#include <iostream>
 
 int main()
 {
-	ABBA o;
-	cout << o.canObtain("B", "ABBA") << "\n";
-	cout << o.canObtain("AB", "ABB") << "\n";
-	cout << o.canObtain("BBAB", "ABABABABB") << "\n";
-	cout << o.canObtain("BBBBABABBBBBBA", "BBBBABABBABBBBBBABABBBBBBBBABAABBBAA") << "\n";
-	cout << o.canObtain("A", "BB") << "\n";
-	return 0;
+    ABBA o;
+    cout << o.canObtain("B", "ABBA") << "\n";
+    cout << o.canObtain("AB", "ABB") << "\n";
+    cout << o.canObtain("BBAB", "ABABABABB") << "\n";
+    cout << o.canObtain("BBBBABABBBBBBA", "BBBBABABBABBBBBBABABBBBBBBBABAABBBAA") << "\n";
+    cout << o.canObtain("A", "BB") << "\n";
+    return 0;
 }
-
+#endif
