099635830dd31e25f23b9a457e20ac8b24310844
Patrick Chin <p.ch@live.co.uk>
2017-09-27 20:51:18 +0100

    Add sparse arrays solution

---
+++ sparse_arrays.cpp
@@ -0,0 +1,31 @@
+
+#include <map>
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int n, q;
+    cin >> n;
+
+    map<string, int> m;
+    for (int i = 0; i < n; i++)
+    {
+        string s;
+        cin >> s;
+        // check if exists?
+        m[s]++;
+    }
+
+    cin >> q;
+    for (int i = 0; i < q; ++i) {
+        string s;
+        cin >> s;
+        cout << m[s] << "\n";
+    }
+
+    return 0;
+}
