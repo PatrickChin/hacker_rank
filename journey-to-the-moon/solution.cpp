31c44b2c15a8117403a8b5c89e606254e6ed9c7a
Patrick Chin <p.ch@live.co.uk>
2017-10-13 20:48:53 +0100

    Improve maths in 'Journey to the Moon'

--- journey-to-the-moon/solution.cpp
+++ journey-to-the-moon/solution.cpp
@@ -5,24 +5,24 @@
 
 using namespace std;
 
-long tree_size(int pos, vector<vector<long>> const& al, vector<bool> &visited)
+int tree_size(int pos, vector<vector<int>> const& al, vector<bool> &visited)
 {
     visited[pos] = true;
-    long size = 1;
-    for (long next : al[pos])
+    int size = 1;
+    for (int next : al[pos])
         if (!visited[next])
             size += tree_size(next, al, visited);
     return size;
 }
 
-vector<long> tree_sizes(vector<vector<long>> const& al)
+vector<int> tree_sizes(vector<vector<int>> const& al)
 {
     vector<bool> visited(al.size());
-    vector<long> sizes;
+    vector<int> sizes;
     for (int i = 0; i < al.size(); i++)
         if (!visited[i])
         {
-            long size = tree_size(i, al, visited);
+            int size = tree_size(i, al, visited);
             sizes.push_back(size);
         }
     return sizes;
@@ -30,26 +30,23 @@ vector<long> tree_sizes(vector<vector<long>> const& al)
 
 int main()
 {
-    long n, p;
+    int n, p;
     cin >> n >> p;
 
-    vector<vector<long>> al(n);
+    vector<vector<int>> al(n);
     for (int i = 0; i < p; i++)
     {
-        long a, b;
+        int a, b;
         cin >> a >> b;
         al[a].push_back(b);
         al[b].push_back(a);
     }
 
-    vector<long> const sizes = tree_sizes(al);
-    long npairs = n*(n-1)/2;
-    for (int i = 0; i < sizes.size(); i++)
-    {
-        long m = sizes[i];
-        npairs -= m*(m-1)/2;
-    }
+    vector<int> const sizes = tree_sizes(al);
+    long npairs = 0;
+    for (int const& m : sizes)
+        npairs += m * (n-m);
 
-    cout << npairs << endl;
+    cout << npairs/2 << endl;
     return 0;
 }
